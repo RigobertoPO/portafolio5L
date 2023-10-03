aaa4ccecd0811469912fcad0d0858055dd437705
rigobertopo <rperez@cobach.edu.mx>
2023-10-03 15:08:00 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi portafolio</title>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <!--Menu-->
+        <nav >
+            <div>
+                <img src="img/logo.png" alt="Logo"/>
+                <ul >
+                    <li ><a href="index.html" class="Seleccionado">Inicio</a></li>
+                    <li><a href="quien.html">Quien soy</a></li>
+                    <li><a href="portafolio.html">PortaFolio</a></li>
+                    <li><a href="blog.html">blog</a></li>
+                    <li><a href="contacto.html">Contactos</a></li>
+                </ul>          
+            </div>
+        </nav> 
+       <div>
+        <div>
+           <h1>Rigoberto Pérez Ovando</h1> 
+           <p >Ingeniero de software</p>
+        </div>
+       </div>
+   </header>
+  
+   <section >
+       <div >
+           <div >
+               <ul >
+                    <li><a href="">Web</a></li>
+                    <li><a href="">Móvil</a></li>
+                    <li><a href="">Escritorio</a></li>
+                    <li><a href="">Diseños</a></li>
+               </ul>
+           </div>
+       </div>
+       <div>
+            <div >
+                <a href="http://">
+                    <img src="img/web.jpg" alt="web">
+                </a>               
+            </div>
+           
+       </div>
+   </section>
+  
+  <footer>
+      <p>universidad</p>
+  </footer>
+</body>
+</html>
