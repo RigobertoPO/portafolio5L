8bf4463db86b252abc4d7fa1e5dd57bbf4eb900e
rigobertopo <rperez@cobach.edu.mx>
2023-10-24 16:23:33 -0600

    Registrar usuarios

--- usuarios/registrarusuario.php
+++ usuarios/registrarusuario.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <section class="h-100 gradient-form" style="background-color: #eee;">
@@ -19,13 +19,18 @@
               <div class="card-body p-md-5 mx-md-4">
 
                 <div class="text-center">
-                  <img src="img/logo.png"
-                    style="width: 185px;" alt="logo">
-                  <h4 class="mt-1 mb-5 pb-1">Iniciar sesión</h4>
+                  <img src="../img/logo.png"
+                    style="width: 65px;" alt="logo">
+                  <h4 class="mt-1 mb-5 pb-1">Registro de nuevo usuario</h4>
                 </div>
                 
-                <form action="usuarios/validarusuario.php" method="post">
-                  <p>Introduce tu cuenta</p>
+                <form action="crearusuario.php" method="post">
+                  <p>Introduce tus datos</p>
+                  <div class="form-outline mb-4">
+                    <input type="text" id="form2Example11" class="form-control"
+                      placeholder="Introduce tu nombre" name="nombre" />
+                    <label class="form-label" for="form2Example11">Nombre</label>
+                  </div>
 
                   <div class="form-outline mb-4">
                     <input type="email" id="form2Example11" class="form-control"
@@ -39,24 +44,13 @@
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <input class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"  type="submit" value="Enviar">
-                    <a class="text-muted" href="#!">Forgot password?</a>
+                    <input class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"  type="submit" value="Crear cuenta">
                   </div>                 
                 </form>
-                <div class="d-flex align-items-center justify-content-center pb-4">
-                    <p class="mb-0 me-2">¿No tienes una cuenta?</p>
-                    <a href="usuarios/registrarusuario.php" class="btn btn-outline-danger">Registrarme</a>
-                  </div>
+                
               </div>
             </div>
-            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">We are more than just a company</h4>
-                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              </div>
-            </div>
+            
           </div>
         </div>
       </div>
